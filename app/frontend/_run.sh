b28462d0fb407db20d25627ac8e78e48fe64e8d4
Okada Hayato <peregrin8alde@gmail.com>
2022-01-28 22:55:58 +0900

    fix: httpd のログ出力
    Refs: #12

--- app/frontend/_run.sh
+++ app/frontend/_run.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+mkdir -p "$PWD/logs"
+
 docker run \
   --rm \
   -dit \
@@ -7,6 +9,7 @@ docker run \
   -p 80:80 \
   -v "$PWD/hdocs":/usr/local/apache2/htdocs/ \
   -v "$PWD/conf/my-httpd.conf":/usr/local/apache2/conf/httpd.conf \
+  -v "$PWD/logs":/usr/local/apache2/logs/ \
   httpd:2.4
 
 echo "http://localhost:80"
