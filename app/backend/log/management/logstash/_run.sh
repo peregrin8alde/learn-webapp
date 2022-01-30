5e227791c11537b91e77364573aaaa882bb50717
Okada Hayato <peregrin8alde@gmail.com>
2022-01-30 11:51:23 +0900

    fix: エージェントからのログ収集
    filebeat を使ったファイルログ収集
    
    Refs: #12

--- app/backend/log/management/logstash/_run.sh
+++ app/backend/log/management/logstash/_run.sh
@@ -1,12 +1,17 @@
 #!/bin/sh
 
+mkdir -p "$PWD/pipeline"
+mkdir -p "$PWD/config"
 mkdir -p "$PWD/logs"
 
 docker run \
   --name logstash \
+  --hostname logstash.sv \
   --rm \
   -it \
+  -p 5044:5044 \
   -v "$PWD/pipeline/":/usr/share/logstash/pipeline/ \
+  -v "$PWD/config/":/usr/share/logstash/config/ \
   -v "$PWD/logs":/logs \
   docker.elastic.co/logstash/logstash:7.16.3
 
