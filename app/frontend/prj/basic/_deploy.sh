c83a4fcac211cfa993c0bc6333faec441602870d
Okada Hayato <peregrin8alde@gmail.com>
2022-02-17 20:03:04 +0900

    fix: ディレクトリ整理

---
+++ app/frontend/prj/basic/_deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE:-$0}")" && pwd)
+PARENT_DIR=$(cd "$(dirname "${BASH_SOURCE:-$0}")/.." && pwd)
+
+DEPLOY_DIR=$(cd "$(dirname "${BASH_SOURCE:-$0}")/../../deploy/hdocs" && pwd)
+
+rm -rf ${DEPLOY_DIR}/*
+
+cp -rf hdocs/* ${DEPLOY_DIR}/
+
+
+exit 0
