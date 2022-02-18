d105bf8360b49447856e442742cf881edaba2265
Okada Hayato <peregrin8alde@gmail.com>
2022-02-18 17:35:00 +0900

    fix: hdocs ごと削除するとサーバー再起動が必要になるため、配下を削除

--- app/frontend/prj/vuejs/prj01/_deploy.sh
+++ app/frontend/prj/vuejs/prj01/_deploy.sh
@@ -5,11 +5,11 @@ PARENT_DIR=$(cd "$(dirname "${BASH_SOURCE:-$0}")/.." && pwd)
 
 BASE_DIR=$(cd "$(dirname "${BASH_SOURCE:-$0}")/../../.." && pwd)
 
-DEPLOY_DIR=${BASE_DIR}/deploy/hdocs
+DEPLOY_DIR=${BASE_DIR}/deploy
 
-rm -rf ${DEPLOY_DIR}
+rm -rf ${DEPLOY_DIR}/hdocs/*
 
-cp -rf hdocs ${DEPLOY_DIR}
+cp -rf hdocs ${DEPLOY_DIR}/
 
 
 exit 0
