160c1b21b32824084c9a5205cd71a12c939a5eac
Okada Hayato <peregrin8alde@gmail.com>
2022-02-27 15:50:04 +0900

    fix: 構成変更

---
+++ src/backend/storage/db/postgres/bin/install.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE:-$0}")" && pwd)
+PARENT_DIR=$(cd "$(dirname "${BASH_SOURCE:-$0}")/.." && pwd)
+
+LOGS_DIR="${PARENT_DIR}/logs"
+PGDATA_DIR="${PARENT_DIR}/data"
+
+mkdir -p "${LOGS_DIR}"
+mkdir -p "${PGDATA_DIR}"
+
+
+exit 0
--- src/backend/storage/db/postgres/bin/start.sh
+++ src/backend/storage/db/postgres/bin/start.sh
@@ -5,12 +5,15 @@ PARENT_DIR=$(cd "$(dirname "${BASH_SOURCE:-$0}")/.." && pwd)
 
 DOCKER_NETWORK="webapp-net"
 
+LOGS_DIR="${PARENT_DIR}/logs"
+PGDATA_DIR="${PARENT_DIR}/data"
+CONFIG_DIR="${PARENT_DIR}/config"
+
 # --filter は golang の正規表現でフィルタ、 -q は id だけ表示
-if [ -z $(docker network ls --filter name="^${DOCKER_NETWORK}$" -q) ]; then
+if [ -z "$(docker network ls --filter name="^${DOCKER_NETWORK}$" -q)" ]; then
   docker network create "${DOCKER_NETWORK}"
 fi
 
-mkdir -p "$SCRIPT_DIR/data"
 
 docker run \
   --name webapp_db \
@@ -20,10 +23,10 @@ docker run \
   -d \
   --user "$(id -u):$(id -g)" \
   -v /etc/passwd:/etc/passwd:ro \
-  -v "$SCRIPT_DIR/data":/var/lib/postgresql/data \
-  -v "$SCRIPT_DIR/conf/postgresql.conf":/etc/postgresql/postgresql.conf \
-  -v "$SCRIPT_DIR/conf/pg_hba.conf":/etc/postgresql/pg_hba.conf \
-  -v "$SCRIPT_DIR/initd":/docker-entrypoint-initdb.d \
+  -v "${PGDATA_DIR}":/var/lib/postgresql/data \
+  -v "${CONFIG_DIR}/postgresql.conf":/etc/postgresql/postgresql.conf \
+  -v "${CONFIG_DIR}/pg_hba.conf":/etc/postgresql/pg_hba.conf \
+  -v "${SCRIPT_DIR}/initd":/docker-entrypoint-initdb.d \
   -e POSTGRES_PASSWORD=postgres \
   -e PGDATA=/var/lib/postgresql/data/pgdata \
   postgres \
