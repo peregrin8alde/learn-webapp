f837fb3e8a20c04074eb0be81082b86513548e92
Okada Hayato <peregrin8alde@gmail.com>
2022-02-27 20:40:36 +0900

    fix: Bootstrap サンプルページの作成
    Refs: #19

---
+++ src/frontend/apps/bootstrap/_build.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE:-$0}")" && pwd)
+PARENT_DIR=$(cd "$(dirname "${BASH_SOURCE:-$0}")/.." && pwd)
+
+TARGET_DIR=${SCRIPT_DIR}/dist
+
+mkdir -p ${TARGET_DIR}
+
+# rsync のオプションは以下の意味
+## -a, --archive : archive mode; equals -rlptgoD (no -H,-A,-X)
+### -r, --recursive : recurse into directories
+### -l, --links : copy symlinks as symlinks
+### -p, --perms : preserve permissions
+### -t, --times : preserve modification times
+### -g, --group : preserve group
+### -o, --owner : preserve owner (super-user only)
+### -D : same as --devices --specials
+## -v, --verbose : increase verbosity
+## -h, --human-readable : output numbers in a human-readable format
+## --exclude=PATTERN : exclude files matching PATTERN
+## --delete : delete extraneous files from dest dirs
+rsync -avh ${SCRIPT_DIR}/htdocs ${TARGET_DIR} --exclude "*.gitkeep" --delete
+
+
+exit 0
---
+++ src/frontend/apps/bootstrap/_deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE:-$0}")" && pwd)
+PARENT_DIR=$(cd "$(dirname "${BASH_SOURCE:-$0}")/.." && pwd)
+
+BASE_DIR=$(cd "${SCRIPT_DIR}/../../../.." && pwd)
+TARGET_DIR=${BASE_DIR}/deploy/frontend/sv/htdocs
+
+mkdir -p ${TARGET_DIR}
+
+rsync -avh ${SCRIPT_DIR}/dist/htdocs/ ${TARGET_DIR} --delete
+
+
+exit 0
