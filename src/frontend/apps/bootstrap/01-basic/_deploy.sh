6d4950311d2e4a4aa583dbb20f802bfd1f3cbde8
Okada Hayato <peregrin8alde@gmail.com>
2022-02-28 20:05:22 +0900

    fix: 構成変更

--- src/frontend/apps/bootstrap/01-basic/_deploy.sh
+++ src/frontend/apps/bootstrap/01-basic/_deploy.sh
@@ -3,7 +3,7 @@
 SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE:-$0}")" && pwd)
 PARENT_DIR=$(cd "$(dirname "${BASH_SOURCE:-$0}")/.." && pwd)
 
-BASE_DIR=$(cd "${SCRIPT_DIR}/../../../.." && pwd)
+BASE_DIR=$(cd "${SCRIPT_DIR}/../../../../.." && pwd)
 TARGET_DIR=${BASE_DIR}/deploy/frontend/sv/htdocs
 
 mkdir -p ${TARGET_DIR}
