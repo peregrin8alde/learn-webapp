41231e481e6e43cf29f0a9ab537176766f88b328
Okada Hayato <peregrin8alde@gmail.com>
2022-03-16 17:50:17 +0900

    fix: docker コンテナ内のユーザ変更

--- src/backend/appsv/apps/python/django/tutorial01/_create_app.sh
+++ src/backend/appsv/apps/python/django/tutorial01/_create_app.sh
@@ -3,14 +3,11 @@
 docker run \
   -it \
   --rm \
+  -u $(id -u):$(id -g) \
   -v "$(pwd)":/usr/src/myapp \
   -w /usr/src/myapp \
   django_base \
     python manage.py startapp polls
-
-# root 権限で作成されるため、権限変更
-## 本来は Docker イメージ作成時点でユーザーを用意しておきたい
-sudo chown -R $(id -u):$(id -g) polls
 
 
 exit 0
--- src/backend/appsv/apps/python/django/tutorial01/_run.sh
+++ src/backend/appsv/apps/python/django/tutorial01/_run.sh
@@ -3,6 +3,7 @@
 docker run \
   -it \
   --rm \
+  -u $(id -u):$(id -g) \
   -p 8000:8000 \
   -v "$(pwd)":/usr/src/myapp \
   -w /usr/src/myapp \
